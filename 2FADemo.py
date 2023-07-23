bd70e6c0a3d59d8bb37523725c6b19b0ee02271d
Václav Berný <vaclav.berny@outlook.cz>
2023-07-23 13:20:04 +0200

    Sending email with pseudo random 6 digit code

---
+++ 2FADemo.py
@@ -0,0 +1,29 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+import random
+
+email_sender = 'bissfit2fa@gmail.com'
+email_password = ''
+email_receiver = 'directspam@outlook.com'
+verification_code = random.randint(99999,1000000)
+
+with open('config') as f:
+    email_password = f.readline()
+
+
+subject = 'New sign in to 2FADemo'
+body = """
+Hi, your verification code is  {}
+""".format(verification_code)
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465,context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
